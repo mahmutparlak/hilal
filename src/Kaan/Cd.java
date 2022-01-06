5589dd8ff0477e1f89aeb1a61928a62a10761dad
mahmutparlak <mahmutparlak@gmail.com>
2022-01-06 03:05:12 -0600

    US-1234 "Login page function test"

--- src/Kaan/Cd.java
+++ src/Kaan/Cd.java
@@ -3,5 +3,6 @@ package Kaan;
 public class Cd {
     public static void main(String[] args) {
         System.out.println("Kaan Cd yi seviyor");
+        System.out.println("canim askim ");
     }
 }
